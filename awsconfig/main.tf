306f29b444fda1b370661cf758dfc90ebfd6e08e
Lara Prieto Alvarez <lara.prieto.alvarez@nttdata.com>
2023-02-09 11:51:47 +0100

    added more rules to aws config module

--- awsconfig/main.tf
+++ awsconfig/main.tf
@@ -1,15 +1,58 @@
+resource "aws_config_configuration_recorder" "secretsmanager_configuration_recorder" {
+  name     = "secretsmanager_configuration_recorder"
+  role_arn = local.aws_service_rol_config
+}
+
 resource "aws_config_config_rule" "secretsmanager_rotation_enabled_check" {
   name = "secretsmanager_rotation_enabled_check"
-
   source {
     owner             = "AWS"
     source_identifier = "SECRETSMANAGER_ROTATION_ENABLED_CHECK"
   }
-
   depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
 }
 
-resource "aws_config_configuration_recorder" "secretsmanager_configuration_recorder" {
-  name     = "secretsmanager_configuration_recorder"
-  role_arn = local.aws_service_rol_config
+resource "aws_config_config_rule" "secretsmanager_rotation_enabled_check" {
+  name = "secretsmanager_rotation_enabled_check"
+  source {
+    owner             = "AWS"
+    source_identifier = "SECRETSMANAGER_ROTATION_ENABLED_CHECK"
+  }
+  depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
+}
+
+resource "aws_config_config_rule" "secretsmanager_rotation_success_check" {
+  name = "secretsmanager_rotation_enabled_check"
+  source {
+    owner             = "AWS"
+    source_identifier = "SECRETSMANAGER_SCHEDULED_ROTATION_SUCCESS_CHECK"
+  }
+  depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
+}
+
+resource "aws_config_config_rule" "secretsmanager_periodic_rotation" {
+  name = "secretsmanager_rotation_enabled_check"
+  source {
+    owner             = "AWS"
+    source_identifier = "SECRETSMANAGER_SECRET_PERIODIC_ROTATION"
+  }
+  depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
+}
+
+resource "aws_config_config_rule" "secretsmanager_secret_unused" {
+  name = "secretsmanager_rotation_enabled_check"
+  source {
+    owner             = "AWS"
+    source_identifier = "SECRETSMANAGER_SECRET_UNUSED"
+  }
+  depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
+}
+
+resource "aws_config_config_rule" "secretsmanager_secret_using_cmk" {
+  name = "secretsmanager_rotation_enabled_check"
+  source {
+    owner             = "AWS"
+    source_identifier = "SECRETSMANAGER_USING_CMK"
+  }
+  depends_on = [aws_config_configuration_recorder.secretsmanager_configuration_recorder]
 }
