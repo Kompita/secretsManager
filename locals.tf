032b59e93747042bf42caed3c8b38047e63b33c1
Lara Prieto Alvarez <lara.prieto.alvarez@nttdata.com>
2023-02-16 13:57:07 +0100

    Changes to be committed: 	Modified:   locals.tf

--- locals.tf
+++ locals.tf
@@ -3,4 +3,13 @@ locals{
         owner = "lara"
         app = "test-correos"
     }
+    secret_id = "test-lara-plain-text-2"
+    secret_value = {
+        engine = "postgres"
+        host = "host"
+        username = "username"
+        password = "password"
+        dbname = "dbname"
+        port = "port"
+    }
 }
