2ec2cd2fa6845586d9fd56cd94e61a88963f72b3
Jon Garcia Beaskoetxea <jgarcbea@emeal.nttdata.com>
2023-02-20 09:44:30 +0100

    remove items for test

--- bdregistro/locals.tf
+++ bdregistro/locals.tf
@@ -1,7 +1,7 @@
 locals {
   master_password      = var.master_password == "" ? random_password.db_pass[0].result : var.master_password
   instance_class       = var.prod ? "db.r5.large" : "db.t3.medium"
-  db_subnet_group_name = var.db_subnet_group_name == "" ? "aw${var.aws_region_id}-${var.environment}-d0-subnet-group-02" : var.db_subnet_group_name
+  # db_subnet_group_name = var.db_subnet_group_name == "" ? "aw${var.aws_region_id}-${var.environment}-d0-subnet-group-02" : var.db_subnet_group_name
   # Inspired from: https://www.reddit.com/r/Terraform/comments/am4d81/conditionally_add_keyvalue_to_map_or_not_at_all/
   optional_tag = {
     prod = {}
