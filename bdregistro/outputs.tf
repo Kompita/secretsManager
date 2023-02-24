36937844e5ee862d0f014dcb608461de2f4afe16
Jon Garcia Beaskoetxea <jgarcbea@emeal.nttdata.com>
2023-02-24 12:13:37 +0100

    change files to dda standard

--- bdregistro/outputs.tf
+++ bdregistro/outputs.tf
@@ -1,25 +1,25 @@
 output "cluster_id" {
-  value = aws_rds_cluster.this.id
+  value = aws_rds_cluster.rds_cluster.id
 }
 
 output "cluster_arn" {
-  value = aws_rds_cluster.this.arn
+  value = aws_rds_cluster.rds_cluster.arn
 }
 
 output "writer_endpoint" {
-  value = aws_rds_cluster.this.endpoint
+  value = aws_rds_cluster.rds_cluster.endpoint
 }
 
 output "reader_endpoint" {
-  value = aws_rds_cluster.this.reader_endpoint
+  value = aws_rds_cluster.rds_cluster.reader_endpoint
 }
 
 output "master_username" {
-  value     = var.master_username
+  value     = aws_rds_cluster.rds_cluster.master_username
   sensitive = true
 }
 
 output "master_password" {
-  value     = local.master_password
+  value     = aws_rds_cluster.rds_cluster.master_password
   sensitive = true
 }
