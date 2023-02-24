4c2395e8e21785d6f8516d515e6f337a9d55d074
Lara Prieto Alvarez <lara.prieto.alvarez@nttdata.com>
2023-02-24 13:56:05 +0100

    modified step functions

--- stepfunctions/locals.tf
+++ stepfunctions/locals.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {} 
 locals {
     account_id = data.aws_caller_identity.current.account_id   
-    aws_service_rol_config = "arn:aws:iam::${account_id}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
+    aws_service_rol_config = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
     tags = {
         owner = "vruizrob"
         app = "step-functions-correos"
